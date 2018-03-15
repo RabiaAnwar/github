ce1eab05fd309376296962f8211898d7d9611d74
Bahria University <spring2018@bahria.edu.pk>
2018-03-15 09:52:49 +0500

    First commit

---
+++ github/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("My name is rabia anwar");
+            Console.WriteLine("I am softwarae engineering");
+        }
+    }
+}
